95437ff4c50b2ab7a2404a303b76e7f72e1a1b53
tmy628 <tmy6h1r@gmail.com>
2021-05-10 06:37:43 +0000

    テストデータの作成 #5

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem "mimemagic", "~> 0.3.10"
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.6.0'
+  gem 'factory_bot_rails', '~> 4.10.0'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13.0'
   gem 'selenium-webdriver'
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  # ユーザー単位では重複したプロジェクト名を許可しないこと
-  it "does not allow duplicate project names per user" do
+  it "ユーザー単位では重複したプロジェクト名を許可しないこと" do
     user = User.create(
       first_name: "Joe",
       last_name:  "Tester",
@@ -22,8 +21,7 @@ RSpec.describe Project, type: :model do
     expect(new_project.errors[:name]).to include("has already been taken")
   end
 
-  # 二人のユーザーが同じ名前を使うことは許可すること
-  it "allows two users to share a project name" do
+  it "二人のユーザーが同じ名前を使うことは許可すること" do
     user = User.create(
       first_name: "Jane",
       last_name:  "Tester",
@@ -48,6 +46,27 @@ RSpec.describe Project, type: :model do
 
     expect(other_project).to be_valid
   end
-end
 
+  describe "遅延ステータス" do
+    it "締切日が過ぎていれば遅延していること" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+
+    it "締切日が今日ならスケジュールどおりであること" do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
+
+    it "締切日が未来ならスケジュールどおりであること" do
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+
+  it "たくさんのメモが付いていること" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
+end
 
